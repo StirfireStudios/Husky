f1f41d27a18f1a6676778f0b07bb21b8ecf895f9
Brendan Ragan <lordmortis@gmail.com>
2013-12-22 20:53:28 +0800

    Add struct for transferring leader board entires and also add callback for sending entries

--- shared/Husky.h
+++ shared/Husky.h
@@ -9,12 +9,20 @@
 #ifndef dummy_Husky_h
 #define dummy_Husky_h
 
+/** Leaderboard entries. TODO: Add opaque type so we can retrieve more info about people **/
+struct HuskyLeaderboardEntry {
+	const char *name;
+	int32_t globalrank;
+	int32_t score;
+};
+
 /** Observers for Husky callbacks **/
 class HuskyObserver {
 public:
-//	virtual ~HuskyObserver();
+	//	virtual ~HuskyObserver();
 	virtual void HuskyObserverAchievementCallback(const char *name, bool success) = 0;
 	virtual void HuskyObserverLeaderboardScoreSetCallback(const char *name, bool success) = 0;
+	virtual void HuskyObserverLeaderboardScoreGetCallback(const char *name, HuskyLeaderboardEntry *entries, int number) = 0;
 };
 
 /** Leaderboard Score keep types **/
@@ -33,7 +41,7 @@ enum HuskyLeaderboardScoreTimeFrame {
 
 class Husky {
 public:
-//	virtual ~Husky();
+	//	virtual ~Husky();
 	
 	/** Get a handle to the husky singleton **/
 	static Husky *getInstance();
@@ -42,10 +50,10 @@ public:
 	static void shutdownInstance();
 	
 	virtual void setObserver(HuskyObserver *observer) = 0;
-
+	
 	/** Try and set the achievement with this name. Will return a setAchievementCallback if an observer is set **/
 	virtual void setAchievement(const char *name) = 0;
-
+	
 	/** This function is run every update cycle to service callbacks etc. */
 	virtual void doTick() = 0;
 	
@@ -54,14 +62,14 @@ public:
 	
 	/** uploads a score to the specified leaderboard **/
 	virtual void uploadLeaderboardScore(const char *name, int32_t score, HuskyLeaderboardScoreToKeep tokeep) = 0;
-
+	
 	/** requests a set of scores from this leaderboard **/
 	/** friends = request only scores that the user is friends with **/
 	/** timeframe = the timeframe to request scores in **/
 	/** offset = the start offset of scores to request **/
 	/** number = the number of scores to request **/
 	virtual void requestLeaderboardScores(const char *name, bool friends, HuskyLeaderboardScoreTimeFrame timeframe, int offset, int number) = 0;
-
+	
 	/** requests a set of scores from this leaderboard around the player **/
 	/** same parameters as above, but offset 0 is the player's current score on this board */
 	/** so offset = -3, would request scores 3 ranks above the player **/
